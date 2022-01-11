8b0ee05490c7620c5360dc8f6a23f89b5f4ec596
Parisa Salmani <55203321+parisasl@users.noreply.github.com>
2022-01-11 20:49:32 +0330

    Update Melborne_HousePrice_Prediction.py

--- Melborne_HousePrice_Prediction.py
+++ Melborne_HousePrice_Prediction.py
@@ -12,7 +12,6 @@ from sklearn.ensemble import RandomForestRegressor #RandomForest model
 melbourne_file_path = '/Users/paris/OneDrive/Desktop/Prog_docum/home-data-for-ml-course/melb_data.csv'
 # read the data and store data in DataFrame titled melbourne_data
 melbourne_data = pd.read_csv(melbourne_file_path) 
-melbourne_data.columns
 
 # print a summary 
 melbourne_data.describe()
